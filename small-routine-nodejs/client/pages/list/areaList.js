15d898d3cd0ac0208f2e07779e54e666d8d09361
darren <darren220@163.com>
2018-08-13 09:38:02 +0800

    FE

---
+++ small-routine-nodejs/client/pages/list/areaList.js
@@ -0,0 +1,129 @@
+// pages/list/areaList.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+    var that = this;
+    wx.request({
+      url: 'http://www.wpd.com/darren/areaList',
+      method: 'GET',
+      data: {},
+      success: function(res) {
+        var list = res.data.areasList;
+        if (null == list) {
+          var toastTitle = '获取列表失败' + res.data.errMsg;
+          wx.showToast({
+            title: toastTitle,
+            icon: '',
+            duration: 2000
+          });
+        } else {
+          that.setData({ // 调用方已发生变化
+            list: list
+          });
+        }
+      }
+    });
+
+  },
+  addArea: function () {
+    wx.navigateTo({
+      url: '../operation/operation',
+    })
+  },
+
+  deleteArea: function (req) {
+    var that = this;
+    wx.showModal({
+      title: '提示',
+      content: '确认删除[' + req.target.dataset.areaname + ']吗？',
+      success: function (sm) {
+        if (sm.confirm) {
+          wx.request({
+            url: 'http://www.wpd.com/darren/deleteArea',
+            method: 'GET',
+            data: {
+              'areaId': req.target.dataset.areaid
+            },
+            success: function (result) {
+              var status = result.data.status;
+              var toastText = "删除成功！";
+              if (status == 'FAIL') {
+                toastText = "删除失败！";
+              } else {
+                that.data.list.splice(req.target.dataset.index, 1)
+                that.setData({
+                  list: that.data.list
+                });
+              }
+              wx.showToast({
+                title: toastText,
+                icon: '',
+                duration: 1000
+              });
+            }
+          });
+        }
+      }
+    });
+  },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+
+
+})
---
+++ small-routine-nodejs/client/pages/swiper/swiper.js
@@ -0,0 +1,86 @@
+// pages/swiper/swiper.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      '/static/images/swiper_01.jpg',
+      '/static/images/swiper_04.jpg',
+      '/static/images/swiper_03.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    prodList:[
+      {
+        logo:'/static/images/cake.jpg',
+        title:'糕点制作',
+        desc: '关键词：蛋糕 甜点 美食 下午茶 水果 慕斯 餐饮美食 西餐美食'
+      },
+      {
+        logo: '/static/images/bottle_wine.jpg',
+        title: '鸡尾酒调制',
+        desc: '关键词：鸡尾酒 预调酒 饮料 冷饮 柠檬 果汁'
+      }
+    ]  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
